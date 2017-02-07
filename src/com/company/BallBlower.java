46372f31345d0d9e9db9fad3c51efab19f447f6a
Tania Ramon <tania.ramon.prats@gmail.com>
2017-02-07 16:30:08 +0100

    Update2

--- src/com/company/BallBlower.java
+++ src/com/company/BallBlower.java
@@ -1,32 +1,27 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class BallBlower {
 
-    private List <Integer> deliveredNumbers = new ArrayList<>();
+    private List <Integer> numbers = new ArrayList<>();
 
-    public Integer entregarNum(){
-
-        while (true) {
-            int randomNum = ThreadLocalRandom.current().nextInt(0, 10);
-
-            if (!deliveredNumbers.contains(randomNum)){
-                deliveredNumbers.add(randomNum);
-                return randomNum;
-            }
+    public BallBlower(){
+        for (int i = 0; i < 10 ; i++) {
+            numbers.add(i);
         }
+        Collections.shuffle(numbers);
+    }
+    public Integer deliveredNumbers(){
+
+        return numbers.remove(numbers.size()-1);
     }
 
     public boolean numbersLeft(){
 
-        for (int i = 0; i <10 ; i++) {
-            if (!deliveredNumbers.contains(i)){
-                return true;
-            }
-        }
-        return false;
+       return numbers.isEmpty();
     }
 }
